4e85dd9340b3f4922b6a1fc295acbbb5f5a6bd46
Ayoub Echcharrat <ayoub.ech-charrat@hotmail.fr>
2023-05-24 15:06:51 +0200

    fix on userdashboard file

--- src/UserDashboard/UserDashbord.jsx
+++ src/UserDashboard/UserDashbord.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React, { useState,useEffect } from 'react';
-import BarChartActivity from './BarChartActivity';
-import { mockedCall, APIcall } from '../APIcalls/APIcalls';
-import KeyDataCard from './KeyDataCard';
-import Score from './Score';
-import ActivityTypes from './ActivityTypes';
-import AverageSessionsDuration from './AverageSessionsDuration';
-import ErrorPage from './Error';
-=======
 //Utils
 import React from "react";
 import { useParams } from "react-router-dom";
@@ -19,7 +9,6 @@ import Score from "./Score";
 import ActivityTypes from "./ActivityTypes";
 import AverageSessionsDuration from "./AverageSessionsDuration";
 import { getUserURL,DataModelization } from "../utility/Index";
->>>>>>> method-Loader-React-router
 
 function UserDashboard() {
   const {id} = useParams()
@@ -35,115 +24,6 @@ function UserDashboard() {
   const dataAverageSessions =new DataModelization(getUserURL(id,'average-sessions'))
   const userAverageSessions = dataAverageSessions.getAverageSessions()
 
-<<<<<<< HEAD
-function getDataOfUser(id) {
-    const typeOfData = ''
-    let getData
-    if (process.env.NODE_ENV === 'development'){
-        getData = mockedCall(id,typeOfData)
-    }else if(process.env.NODE_ENV === 'production'){
-        getData = APIcall(id,typeOfData)
-    }
-    return getData
-}
-
-
-function getTypeOfActivity(id){
-    const typeOfData = 'performance'
-    let TypeOfActivity 
-    if (process.env.NODE_ENV === 'development'){
-        TypeOfActivity = mockedCall(id,typeOfData)
-    }else if(process.env.NODE_ENV === 'production'){
-        TypeOfActivity = APIcall(id,typeOfData)
-    }
-
-    return TypeOfActivity
-}
-
-
-function getAverageActivity(id){
-    const typeOfData = 'average-sessions'
-    let averageActivity 
-    if (process.env.NODE_ENV === 'development'){
-        averageActivity = mockedCall(id,typeOfData)
-    }else if(process.env.NODE_ENV === 'production'){
-        averageActivity = APIcall(id,typeOfData)
-    }
-
-    return averageActivity
-}
-
-
-/* function CheckIfIdCorrect(){
-    const pathArray = getCurrentURL()
-    const id = Number(pathArray[1])
-    const typeOfData = ''
-    let isIdCorrect
-    if (process.env.NODE_ENV === 'development'){
-        isIdCorrect = mockedCall(id,typeOfData)
-    }else if(process.env.NODE_ENV === 'production'){
-        console.log(process.env.NODE_ENV)
-        isIdCorrect = APIcall(id,typeOfData)
-    }
-        return isIdCorrect ? UserDashboard(id) : <ErrorPage/>
-} */
-
-
-
-function CheckIfIdCorrect(){
-    const [call,UseCall] = useState([])
-
-    const pathArray = getCurrentURL()
-    const id = Number(pathArray[1])
-    const typeOfData = ''
-    let isIdCorrect
-
-    const getData = () => {APIcall(id,typeOfData).then(call => UseCall(call))}
-    console.log(call)
-    
-
-    return isIdCorrect ?   console.log(isIdCorrect)  : <ErrorPage/> //UserDashboard(id)
-}
-
-
-
-
-function UserDashboard(id) {
-    const dataOfUser = getDataOfUser(id)
-    const userActivity = getUserActivity(id)
-    const nameOfUser = dataOfUser.data.userInfos.firstName
-    const TypeOfActivity = getTypeOfActivity(id)
-    const averageActivity = getAverageActivity(id)
-
-    return (
-        <div className='dashboard-container'>
-            <h1 className='dashboard-title'>Bonjour <span style={{color:'red'}}>{nameOfUser}</span></h1>
-            <div className="left-block">
-                <BarChartActivity userActivity={userActivity} />
-                <div className="sub-activity-container">
-                    <AverageSessionsDuration averageActivity={averageActivity} />
-                    <ActivityTypes TypeOfActivity={TypeOfActivity}/>
-                    <Score dataOfUser={dataOfUser} />
-                </div>
-            </div>
-            <div className="right-block">
-                <div className="keyData-card-container">
-                    <KeyDataCard type='Calories' >
-                        {dataOfUser.data.keyData.calorieCount.toLocaleString("en-US")}
-                    </KeyDataCard>
-                    <KeyDataCard type='Protéines' >
-                        {dataOfUser.data.keyData.proteinCount.toLocaleString("en-US")}
-                    </KeyDataCard>
-                    <KeyDataCard type='Glucides' >
-                        {dataOfUser.data.keyData.carbohydrateCount.toLocaleString("en-US")}
-                    </KeyDataCard>
-                    <KeyDataCard type='Lipides' >
-                        {dataOfUser.data.keyData.lipidCount.toLocaleString("en-US")}
-                    </KeyDataCard>
-                </div>
-            </div>
-            
-=======
   const dataPerformance = new DataModelization(getUserURL(id,'performance'))
   const userPerformance = dataPerformance.getPerformance()
 
@@ -159,7 +39,6 @@ function UserDashboard(id) {
           <AverageSessionsDuration userAverageSessions={userAverageSessions} />
           <ActivityTypes userPerformance={userPerformance} />
           <Score score={score} />
->>>>>>> method-Loader-React-router
         </div>
       </div>
       <div className="right-block">
